c16478cb919439b48827446c8476f1e1aa78dc34
YouchengZHANG <36500121+YouchengZHANG@users.noreply.github.com>
2018-03-11 10:18:52 +0100

    Add Fasta_Separater.py script
    Fasta_Separater.py: This python script when runs takes the dataset and outputs every protein and its sequence to one individual file

---
+++ scripts/Fasta_Separater.py
@@ -0,0 +1,34 @@
+
+
+
+#  This python script when runs takes the dataset and outputs every protein and its sequence to one individual file
+#  The outputs are .fasta format
+#  This script is for further running PSI-BLAST to get homologs of each protein/sequence
+#  Input the dataset file below
+#  Change the path you want to save the output fasta files (Default: same path/folder of the input dataset file)
+
+
+def Fasta_Separater(filename):
+    f = open(filename,'r+')
+    fr = f.readlines()
+    for i in range(0,len(fr),3):
+       fasta_name = fr[i].lstrip('>').rstrip('\n')
+       #path = 'PATH' + str(fasta_name) + '.fasta'
+       o = open(fasta_name + '.fasta','w')
+       o.write(fr[i]+fr[i+1])
+       o.close()
+    f.close()
+
+
+if __name__ == '__main__':
+    from datetime import datetime
+    start_time = datetime.now()
+    print('Program is running...')
+    
+    #input = ('Input filename: ')
+    Fasta_Separater('300train_ts_red_70.txt')
+
+    end_time = datetime.now()
+    print('Starting from',start_time,'to',end_time)
+    print('Running Time: {}'.format(end_time - start_time))
+
