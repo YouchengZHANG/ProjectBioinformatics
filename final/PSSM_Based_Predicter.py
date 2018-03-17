8e12fd5103c767df3a2562ccbc7d5b2c9f98bacc
YouchengZHANG <36500121+YouchengZHANG@users.noreply.github.com>
2018-03-17 02:58:44 +0100

    Update PSSM_Based_Predictor

--- final/PSSM_Based_Predicter.py
+++ final/PSSM_Based_Predicter.py
@@ -52,21 +52,21 @@ for i in range(0,len(f),2):
 fr.close()
 
 
-#  STEP 4: Build AA_table that convert each amino acid in each sequence into integer form
-#  Create a string with twenty '0' and convert into list as [0,0,0,...,0,0,0]
+#  STEP 5: Build AA_table that convert each amino acid in each sequence into integer form
 #  Twentry common amino acid 'ARNDCQEGHILKMFPSTWYV'
 print('Creating tables...')
 AA_table = Table_Creater.Table_Creater()[0]
 
 
-#  STEP 5: Build int_stc_table that convert each integer form to the structure
+#  STEP 6: Build int_stc_table that convert each integer form to the structure
 #  Example:  int_stc_table[1] = S; stc_table[0] = .
 int_stc_table = Table_Creater.Table_Creater()[3]
 
 
-#  STEP x: Create *_result folder to save predicted output (* refers to the name of unknown fasta file )
+#  STEP 7: Create *_result folder to save predicted output (* refers to the name of unknown fasta file )
 #  './*_result/fasta/' : stores individual .fasta files separated from original unknown fasta file
 #  './*_result/pssm/' : saves the .pssm files searching from PSI-BLAST
+#  './*_result/align/' : saves the .align files from PSI-BLAST
 print('Creating _result folder')
 fasta_file = sys.argv[1]
 unknown_fasta = os.path.splitext(fasta_file)[0]
@@ -79,7 +79,7 @@ dir_align = './' + str(unknown_fasta) + '_result/align/'
 os.makedirs(dir_align,exist_ok=True)
 
 
-#  STEP x: Run Module: Predict_Separater
+#  STEP 8: Run Module: Predict_Separater
 #  This modules when runs takes each unknown sequence in the fasta file 
 #  and outputs each protein and its sequence to each separated single .fasta files
 print('Separating input fasta file...')
@@ -87,7 +87,7 @@ print('Creating single sequence fasta file...')
 Predict_Separater.Predict_Separater(fasta_file, dir_fasta)
 
 
-#  STEP x: Run Module: Predict_Preprocessor 
+#  STEP 9: Run Module: Predict_Preprocessor 
 #  This module when runs takes each .fasta files in fasta folder (Default: dir_fasta) to PSIBLAST 
 #  And outputs .pssm files to pssm folder (Default: dir_pssm)
 #  dir_fasta = './' + str(unknown_fasta) + '_result/fasta/'
@@ -96,7 +96,7 @@ Predict_Separater.Predict_Separater(fasta_file, dir_fasta)
 Predict_Preprocessor.Predict_Preprocessor(dir_db,dir_fasta)
 
 
-#  STEP 6: Run Module: Predict_PSSM_Processor 
+#  STEP 10: Run Module: Predict_PSSM_Processor 
 #  Predict the sequence and convert integer output to character form using int_stc_table
 print('Start processing the window...')
 print('Window size: [j-18 ... j .. j+2]')
@@ -115,7 +115,7 @@ for names in test_pid_all:
 #print(test_stc_all)
 
 
-#  STEP 9: Save the predicted output to file
+#  STEP 11: Save the predicted output to file
 fo = open(dir_result + 'Predict_Result.txt','w')
 for i in range(len(test_pid_all)):
     fo.write('>' + test_pid_all[i] + '\n')
